3a02f0a8e3a3356a32b6f9014c89033650162187
Whitney Jenich <wjenich@gmail.com>
2015-10-21 15:11:44 -0500

    Checkpoint 29 completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,14 +1,27 @@
- var animatePoints = function() {
+$(window).load(function() {
+          if ($(window).height() > 950) {
+         animatePoints();
+     }
+var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+           
+$(window).scroll(function(event) {
+    if ($(window).scrollTop() >= scrollDistance) {
+        animatePoints();
+         }
+     });
+});
 
-    var points = document.getElementsByClassName('point');
-     
-    var revealPoint = function(index) {
-        points[index].style.opacity = 1;
-        points[index].style.transform = "scaleX(1) translateY(0)";
-        points[index].style.msTransform = "scaleX(1) translateY(0)";
-        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";   
+
+var animatePoints = function() {
+     var revealPoint = function() {
+         $(this).css({
+             opacity: 1,
+             transform: 'scaleX(1) translateY(0)'
+         });
     };
      
-    for (var i = 0; i < point.length; i++) {
-            revealPoint(i);
+    $.each($('.point'), revealPoint);
 };
+     
+
+ 
